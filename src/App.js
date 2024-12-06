1c97c0ea9b7163d1ee0eb72193a831cf6067d1e2
maedot47 <167564056+maedot47@users.noreply.github.com>
2024-12-06 09:09:56 -0500

    main screen

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-//import logo from './logo.svg';
-//import './App.css';
-
-//function App() {
-  //return (
-   //<h1 > hellow world</h1>
-   
- // );
-//}
-
-//export default App;
-import React from 'react';
-import './App.css';
-import { DefaultButton } from '@fluentui/react';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="container">
-      <div className="search-bar">
-        <input type="text" placeholder="Search" />
-      </div>
-      <div><DefaultButton text="Click me" onClick={() => alert('Default Button Clicked!')} />
-      </div>
-      <div className="content">
-        <div className="left-panel">
-          <p>Content on the left</p>
+      <header>
+        <div className="header-left">
+          <span className="username">BigBob</span>
         </div>
-        <div className="right-panel">
-          <p>Content on the right</p>
+        <div className="header-right">
+          <span className="app-title">VolleyVibe</span>
         </div>
-      </div>
+      </header>
+
+      <main>
+        <div className="sidebar">
+          <div className="group-search">
+            <input type="text" placeholder="Group Name" className="search-input" />
+          </div>
+          <span className="empty-text">Empty</span>
+          <div className="group-item">
+            <div className="group-avatar">
+              <img src="avatar1.png" alt="Avatar 1" />
+              <img src="avatar2.png" alt="Avatar 2" />
+            </div>
+            <span className="group-name">+1 BobGang</span>
+          </div>
+          <button className="find-more-button">Find more matches</button>
+        </div>
+        <div className="main-content">
+          {/* Main content is now correctly stretching */}
+        </div>
+      </main>
     </div>
   );
 }
